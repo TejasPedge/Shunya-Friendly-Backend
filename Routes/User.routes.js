746fcc083f679a451cec843ba7bb15a44ec06a99
tejaspedge <tejaspedge512@gmail.com>
2023-07-09 15:32:51 +0530

    All Routes completed

--- Routes/User.routes.js
+++ Routes/User.routes.js
@@ -43,26 +43,45 @@ UserRouter.post('/post', async (req,res) => {
         if (error.code === 11000) {
             return res.status(400).send({error : "User with this email id already exists"})
         }
-        res.status(500).send({ error: error });
+        res.status(500).send({ error: 'Server Error' });
     }
 });
 
 //this Route used to Update users Data to the Database
 UserRouter.put('/put/:user_id', async (req,res) => {
-
     try {
+        const {user_id} = req.params;
+        const user = req.body;
+        const update_user = await USER_MODEL.findByIdAndUpdate(user_id,user);
+
+        if (!update_user) {
+            return res.status(404).send({ error: 'User not found' });
+        }
         
+        res.send({message : 'User updated succesfully ', updatedUser : update_user});
+
     } catch (error) {
-        
+        console.error('Unable to update the user:', error);
+        res.status(500).send({ error: 'Server Error' });
     }
-
-
 });
 
 //this Route used to Delete users Data from the Database
 UserRouter.delete('/delete/:user_id', async (req,res) => {
+    try {
+        const {user_id} = req.params;
+        const user = req.body;
+        const delete_user = await USER_MODEL.findByIdAndDelete(user_id);
 
+        if(!delete_user) {
+            return res.status(404).send({ error: 'User not found' });
+        }
 
+        res.send({message : 'User deleted successfully', deleted_uSer : delete_user})
+    } catch (error) {
+        console.error('Unable to Delete the user:', error);
+        res.status(500).send({ error: 'Server Error' });
+    }
 });
 
 
